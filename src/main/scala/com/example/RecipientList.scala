02b4a580880001dff67e125415031047afd73c0e
shigemk2 <i.am.shige@gmail.com>
2016-10-26 22:13:54 +0900

    Add case classes

--- src/main/scala/com/example/RecipientList.scala
+++ src/main/scala/com/example/RecipientList.scala
@@ -3,5 +3,17 @@ package com.example
 import akka.actor._
 import com.example._
 
+case class RequestForQuotation(rfqId: String, retailItems: Seq[RetailItem]) {
+  val totalRetailPrice: Double = retailItems.map(retailItem => retailItem.retailPrice).sum
+}
+
+case class RetailItem(itemId: String, retailPrice: Double)
+
+case class PriceQuoteInterest(path: String, quoteProcessor: ActorRef, lowTotalRetail: Double, highTotalRetail: Double)
+
+case class RequestPriceQuote(rfqId: String, itemId: String, retailPrice: Double, orderTotalRetailPrice: Double)
+
+case class PriceQuote(rfqId: String, itemId: String, retailPrice: Double, discountPrice: Double)
+
 object RecipientListDriver {
 }
